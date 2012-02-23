a4948f0c2f837bb9191fee7524457ba2becb2be2
Jacob Parker <j3parker@csclub.uwaterloo.ca>
2012-02-22 22:43:07 -0500

    ported more generalized handler stuff from csc fork

--- src/config.d
+++ src/config.d
@@ -3,9 +3,9 @@ const string url_root = "/~j3parker/";
 const string site_title = "This is a Title";
 const string site_subtitle = "but this is a subtitle";
 
-const bool nav_tree_vert = true;
-const bool nav_tree_chev = true;
-const bool page_container = false;
+const bool nav_tree_vert = false;
+const bool nav_tree_chev = false;
+const bool page_container = true;
 
 string[string] handlers;
 
